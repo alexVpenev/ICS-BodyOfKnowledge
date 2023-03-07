b4fdbe37c5a26e3f34e4a96332f5285c3e44e6d8
alexVpenev <penev027@gmail.com>
2023-03-07 18:12:13 +0100

    security threats topic added

--- src/components/navigation-pages.php
+++ src/components/navigation-pages.php
@@ -14,17 +14,23 @@
   <button class="navbar-toggle" onclick="toggleNav()">☰</button>
   <nav class="navbar-links">
     <ul>
-      <li><a href="../../index.php">Home</a></li>
-      <li><a href="../pages/introduction.php">Introduction</a></li>
-      <li><a href="../pages/versionHistory.php">Version Control</a></li>
-      <li><a href="#">Introduction into IT Security</a></li>
-      <li class="subsection"><a href="../pages/basic-hacking-and-pentesting-process.php">Basic Hacking and Pentesting Process</a></li>
-      <li class="subsection"><a href="../pages/threat-and-risk-analysis-pt1-cia.php">Threat & Risk Analysis Part 1</a></li>
-      <li><a href="#">Web Application Attack and Defense</a></li>
-      <li class="subsection"><a href="#">Install Vulnerable Web App</a></li>
-      <li class="subsection"><a href="#">Footprinting, Reconnaissance and Social Engineering</a></li>
-      <li><a href="#">Web Site Attack and Defense</a></li>
-      <li class="subsection"><a href="#">Path Traversal, (remote) File inclusion and Command Injection</a></li>
+      <li class="common"><a href="../../index.php">Home</a></li>
+      <li class="common"><a href="../pages/introduction.php">Introduction</a></li>
+      <li class="common"><a href="../pages/versionHistory.php">Version Control</a></li>
+
+      <li class="section1"><a href="#">Web Application Security</a></li>
+
+      <li class="subsection1"><a href="#">Introduction into IT Security</a></li>
+      <li class="subsubsection1"><a href="../pages/basic-hacking-and-pentesting-process.php">Basic Hacking and Pentesting Process</a></li>
+      <li class="subsubsection1"><a href="../pages/threat-and-risk-analysis-pt1-cia.php">Threat & Risk Analysis Part 1</a></li>
+
+      <li class="subsection1"><a href="#">Web Application Attack and Defense</a></li>
+      <li class="subsubsection1"><a href="#">Install Vulnerable Web App</a></li>
+      <li class="subsubsection1"><a href="#">Footprinting, Reconnaissance and Social Engineering</a></li>
+
+      <li class="subsection1"><a href="#">Web Site Attack and Defense</a></li>
+      <li class="subsubsection1"><a href="#">Path Traversal, (remote) File inclusion and Command Injection</a></li>
+
     </ul>
   </nav>
 </div>
--- src/pages/threat-and-risk-analysis-pt1-cia.php
+++ src/pages/threat-and-risk-analysis-pt1-cia.php
@@ -1,4 +1,5 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,10 +10,89 @@
     ?>
     <link rel="stylesheet" href="template.css">
 </head>
+
 <body>
     <header>
         <h1>Threat & Risk Analysis part 1</h1>
     </header>
-    
+    <div class="container">
+        <p>In this topic I'm going to ignore the hackers and the type of hackers there exist, and I am going to focus on the different types of hacks
+            bad individuals will try to carry out on systems. Afterwards I'm going to focus on how a cyber security expert can determine which parts
+            of the systems he is maintaining can be compromised using the <b>CIA triad</b> and determine the probability and impact they can leave.</p>
+        <div class="subtheme">Kinds of Security Threats</div>
+        <p>
+            As technology continues to advance, the threat of cyber attacks is becoming increasingly prevalent.
+            Cyber attacks are malicious attempts by hackers to breach a system and obtain sensitive information or exploit the system for their benefit.
+            In this document, we will discuss the various types of cyber threats that exist and how they can impact our systems.
+        </p>
+
+        <div class="bullet">
+            <ul>
+                <li><b>Malware (virus, trojan horse, worms, etc.)</b> Malware is a type of software that is designed to
+                    infiltrate a system without the user's knowledge.
+                    This can lead to the installation of malicious software or the stealing of sensitive information.
+                </li>
+
+                <li><b>Spam</b>: Spam is unsolicited email or messages that are sent in bulk to a large number of recipients.
+                    It is often used as a method for distributing malware or phishing attacks.
+                </li>
+
+                <li><b>Phishing</b>: Phishing is a type of cyber attack that involves the use of fraudulent emails or
+                    messages to trick users into giving up their personal information, such as usernames and passwords.
+                </li>
+
+                <li><b>Spear phishing</b>: Spear phishing is a more targeted version of phishing, where the attacker
+                    specifically targets individuals or organizations with personalized messages that appear to be from a trusted source.
+                </li>
+
+                <li><b>Adware</b>: Adware is software that displays unwanted advertisements to users.
+                    While it may not be inherently malicious, it can still be a nuisance and can lead to the installation of other unwanted software.
+                </li>
+
+                <li><b>Man-in-the-middle attack</b>: A man-in-the-middle attack occurs when a hacker intercepts communication
+                    between two parties, allowing them to eavesdrop on conversations or steal sensitive information.
+                </li>
+
+                <li><b>Ransomware</b>: Ransomware is a type of malware that encrypts a user's files and demands a ransom payment in exchange for the decryption key.
+                </li>
+
+                <li><b>Denial of Service (DOS and DDOS)</b>: DOS and DDOS attacks are designed to overwhelm a system with traffic,
+                    making it unusable for legitimate users.
+                </li>
+
+                <li><b>Advanced persistent threats</b>: Advanced persistent threats are a type of cyber attack that involves a
+                    prolonged and targeted effort to breach a system, often by a well-funded and organized group.
+                </li>
+
+            </ul>
+
+            <div class="subtheme">The CIA triad</div>
+
+            <div class="image">
+            <img src="../assets/threat-risk-analysis-pt1/cia-triad.png" alt="cia-triad">
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
+
 </html>
