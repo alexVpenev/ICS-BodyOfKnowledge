85e0aefb347ff75795b3dba305d4ea54170bc01a
alexVpenev <penev027@gmail.com>
2023-04-06 19:47:33 +0200

    progress on network scanning

--- src/pages/network-scanning-enumeration-sniffing.php
+++ src/pages/network-scanning-enumeration-sniffing.php
@@ -25,9 +25,8 @@
 
     <div class="container">
 
-        <p>
-            This is the first topic I'll cover out of the Network Security theme. Here I will begin by covering the concept of what Network Scanning is, 
-            I will try to go more in depth into the topic and the technology that surrounds it. I will also describe the scanning process of a network, 
+        <p>This is the first topic I'll cover out of the Network Security theme. Here I will begin by covering the concept of what Network Scanning is,
+            I will try to go more in depth into the topic and the technology that surrounds it. I will also describe the scanning process of a network,
             and finally show some examples of how scanning works along with some final thoughts.
         </p>
 
@@ -38,11 +37,55 @@
 
         <div class="subtheme">Networks, Hosts, Devices and CKC Reconnaissance</div>
 
-        <p>
-            Since this is the first topic of the new Network Security theme, I wanted to delve into the basics of networks, hosts and devices, 
-            before heading in the security. To describe a 
+        <p>Since this is the first topic of the Network Security theme, I wanted to delve into the basics of what a network is, before heading
+            into the topic of Scanning and Ennumeration. What we call a network is actually just a collection of devices that can communicate
+            between eachother. Hosts essentially are devices that are in a network(either private or public) and they all have unique
+            identifiers(what we call ip address). For Example, 192.168.0.1 is a common private IP addres used for a router, while 192.168.0.2
+            could be a device like a laptop, phone, smart watches etc. The server on which this website is hosted(a AWS Cloud Server) has the
+            public IP address 143.42.53.121.
+            </br></br>
+            Another vital part of networks that I will cover more extensivly in the following BoK topics are the hardware and software
+            components, these are routers, switches and firewalls. In simple terms routers are hosts that connect the outside network and a private one.
+            The devices connected to the router have different addresses given out by the router, and the router is responsible for routing data between different
+            devices. Switches are used to connect multiple devices within a network. Finally we have softwares, firewall is a type of software that filters
+            incoming and outgoing traffic.
+            </br></br>
+            Finally the last important aspect of a network security is understanding the different ports that are used by network hosts and devices.
+            Ports are used to enable different types of network communications. For example HTTP traffic used for web browsing(as you can see this php website uses port 2999)
+            typically uses port 80, HTTPS traffic(same as HTTP traffic, but uses RSA encyption) uses port 443. Other common ports are 21(FTP), 22(SSH) and
+            23(Telnet).
 
         </p>
+
+        <div class="subtheme">Network Scanning and Enumeration</div>
+
+        <p>The process of Netwrok scanning is essentially finding and identifying active hosts and open ports on a network.
+            Network enumeration builds upon scanning, by gathering <b>information</b> about the hosts and services they are running i.e
+            the operating system of the host, and the services on ports.
+            </br></br>
+            Network scanning and enumeration is a process from the 'Reconnaissance' phase from the CKC that allows all actors to obtain data
+            and information on their targeted systems. By establishing a connection to perform different scans and queries utilising bultiple tools,
+            the actors are able to learn about all of the exposed ports, connections, hosts, firewalls, system data...etc.
+            </br></br>
+            There are several tools and technologies that can be used for network scanning and enumeration, such as Nmap, a popular
+            open-source network mapping tool, and Metasploit, a penetration testing framework that includes a variety of scanning
+            and enumeration modules.
+            </br></br>
+            In the following examples, I will show different scanning and enumeration techniques in more detail, and will give an explainations
+            on how they can be used to identify vulnerabilities and improve security.
+        </p>
+
+        <div class="subtheme">Examples and Exercises</div>
+
+        <p>
+
+        </p>
+        <!-- 
+        <div class="subtheme">Afterthoughts</div>
+
+        <p>
+
+        </p> -->
 
     </div>
 
